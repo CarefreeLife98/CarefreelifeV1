9a7a7b793af0eef179bdaf06c30aca25a1a429a6
CarefreeLife98 <125889286+CarefreeLife98@users.noreply.github.com>
2023-03-12 14:39:30 +0900

    New Start
    Basic Web Server Application (SpringMVC, CRUD, h2Database)

---
+++ carefreelifeV1/src/main/java/CFL/carefreelifeV1/domain/item/ItemRepository.java
@@ -0,0 +1,14 @@
+//package CFL.carefreelifeV1.domain.item;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Repository;
+//
+//@Slf4j
+//@Repository
+//public class ItemRepository {
+//
+//    Item item = new Item();
+//
+//
+//
+//}
